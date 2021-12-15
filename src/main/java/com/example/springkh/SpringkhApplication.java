5ee464a71f189eb7d440a00240a5b832a359c37c
omar saeed <os83575@gmail.com>
2021-12-15 10:57:17 +0200

    spring kh

---
+++ src/main/java/com/example/springkh/SpringkhApplication.java
@@ -0,0 +1,14 @@
+package com.example.springkh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringkhApplication.class, args);
+    }
+
+
+}
---
+++ src/main/java/com/example/springkh/controller/Testingcontroller.java
@@ -0,0 +1,17 @@
+package com.example.springkh.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//http://localhost:8072/
+@RequestMapping("/api")
+//http://localhost:8072/api
+public class Testingcontroller {
+    //http://localhost:8072/api/name
+    @GetMapping ("/name")
+    public String getname(){
+        return "omar saeed " ;
+    }
+}
